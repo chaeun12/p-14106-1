68682f7b5cd0bd354eb0e2282b20997fe2a115d9
chaeun12 <145729167+chaeun12@users.noreply.github.com>
2025-12-11 12:14:45 +0900

    step 1

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { apiFetch } from "@/lib/backend/client";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
+
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = use(params);
+  const router = useRouter();
+
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>([]);
+
+  const deletePost = (id: number) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+      router.replace("/posts");
+    });
+  };
+
+  useEffect(() => {
+    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+
+    apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments);
+  }, []);
+
+  if (post == null) return <div>로딩중...</div>;
+
+  return (
+    <>
+      <h1>글 상세페이지</h1>
+
+      <div>번호 : {post.id}</div>
+      <div>제목: {post.title}</div>
+      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+      <div className="flex gap-2">
+        <button
+          className="p-2 rounded border"
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
+        >
+          삭제
+        </button>
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
+          수정
+        </Link>
+      </div>
+
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { apiFetch } from "@/lib/backend/client";
+import type { PostDto } from "@/type/post";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [posts, setPosts] = useState<PostDto[]>([]);
+
+  useEffect(() => {
+    apiFetch(`/api/v1/posts`).then(setPosts);
+  }, []);
+
+  return (
+    <>
+      <h1>글 목록</h1>
+
+      {posts.length == 0 && <div>로딩중...</div>}
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              {post.id} : {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <Link href="/posts/write">글쓰기</Link>
+      </div>
+    </>
+  );
+}
